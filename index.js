5106f341629d73913728cf7aeeb4f1f2eb0ddbd2
larcdian <larcdian@gmail.com>
2024-01-10 07:24:24 -0300

    chore: added dark mode and link nav focus selected

---
+++ index.js
@@ -0,0 +1,36 @@
+const links = document.getElementsByClassName('nav-link');
+const codes = document.getElementsByClassName('header-code');
+const toggle = document.getElementById('darkmode-container');
+const body = document.querySelector('body');
+
+
+// darkmode toggle
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function() {
+        removeClass();
+        this.classList.add('selected');
+    });
+}
+
+function removeClass() {
+    for (let i = 0; i < links.length; i++) {
+        links[i].classList.remove('selected');
+    }
+}
+
+
+
+
+    
+    toggle.onclick = () => {
+        toggle.classList.toggle('active');
+        body.classList.toggle('active');
+        for (let i = 0; i < codes.length; i++) {
+                codes[i].classList.toggle('active');
+            }
+        }
+    // for (let i = 0; i < codes.length; i++) {
+    //         this.classList.add('active');
+    // }
+
