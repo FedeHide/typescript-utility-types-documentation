9bccb099eae85065b35d39c77d8ebd1f00dba135
larcdian <larcdian@gmail.com>
2024-01-10 23:37:04 -0300

    chores: adding releases boxes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const links = document.getElementsByClassName('nav-link');
 const codes = document.getElementsByClassName('header-code');
+const releaseBox = document.getElementsByClassName('release-box');
 const toggle = document.getElementById('darkmode-container');
 const body = document.querySelector('body');
 
@@ -29,8 +30,8 @@ function removeClass() {
         for (let i = 0; i < codes.length; i++) {
                 codes[i].classList.toggle('active');
             }
+        for (let i = 0; i < releaseBox.length; i++) {
+            releaseBox[i].classList.toggle('active');
+            }
         }
-    // for (let i = 0; i < codes.length; i++) {
-    //         this.classList.add('active');
-    // }
 
